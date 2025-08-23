18bd09a8b8abc63fafb6204e0bc3a7466c2946e6
Elif Özge <ozgevurmaz@gmail.com>
2025-08-23 21:27:31 +0300

    tailwind entegrated

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
 import { Pressable } from 'react-native';
-
+import "../../global.css"
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
